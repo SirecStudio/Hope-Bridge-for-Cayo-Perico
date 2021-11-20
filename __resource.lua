f05ceda138e36c3d7fabe704e9e5aafc9289a361
SirecStudio <sirecstudio.net@gmail.com>
2021-11-20 21:15:49 +0200

    Release 1.0
    Release

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/extraprops_def.ytyp'
+--data_file 'DLC_ITYP_REQUEST' 'stream/bridge_part.ytyp'
+
+--Make By Sirec Studio !
+
+--DISCORD - https://discord.gg/XJrjmXc9hd ! For Support and Others !
+
+--BUY ME A COFFEE - https://paypal.me/sirecstudio !
